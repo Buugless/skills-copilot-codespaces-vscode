180e070258309018e2d540a9ee55613fa04508a6
Buugless <115627488+Buugless@users.noreply.github.com>
2024-06-10 15:58:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,84 +1,80 @@
-// Create a web server
-// Create a web server
+// Create web server
 const express = require('express');
 const app = express();
-const port = 3000;
 
-// Load the comments module
-const comments = require('./comments');
+// Use the express.json middleware to parse JSON data from the request body
+app.use(express.json());
 
-// Load the comments module
-const comments = require('./comments');
+// Create an array to store comments
+const comments = [];
 
-// Get all comments
+// Create a POST route to add a comment
+app.post('/comments', (req, res) => {
+  // Get the comment data from the request body
+  const comment = req.body;
+  // Add the comment to the comments array
+  comments.push(comment);
+  // Send a response with the comment data
+  res.send(comment);
+});
+
+// Create a GET route to get all comments
 app.get('/comments', (req, res) => {
-  res.json(comments);
+  // Send a response with the comments array
+  res.send(comments);
 });
 
-// Get a single comment
-app.get('/comments/:id', (req, res) => {
-  const comment = comments.find(comment => comment.id === parseInt(req.params.id));
-  
-  if (!comment) {
-    res.status(404).send('The comment with the given ID was not found.');
-  } else {
-    res.json(comment);
-  }
+// Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Path: index.js
+// Import the express module
+const express = require('express');
+// Create an express application
+const app = express();
+
+// Use the express.json middleware to parse JSON data from the request body
+app.use(express.json());
+
+// Create an array to store comments
+const comments = [];
+
+// Create a POST route to add a comment
+app.post('/comments', (req, res) => {
+  // Get the comment data from the request body
+  const comment = req.body;
+  // Add the comment to the comments array
+  comments.push(comment);
+  // Send a response with the comment data
+  res.send(comment);
 });
-// Get all comments
+
+// Create a GET route to get all comments
 app.get('/comments', (req, res) => {
-  res.json(comments);
+  // Send a response with the comments array
+  res.send(comments);
 });
 
-// Get a single comment
-app.get('/comments/:id', (req, res) => {
-  const comment = comments.find(comment => comment.id === parseInt(req.params.id));
-  
-  if (!comment) {
-    res.status(404).send('The comment with the given ID was not found.');
-  } else {
-    res.json(comment);
+// Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
+
+// Path: package.json
+{
+  "name": "express-comments",
+  "version": "1.0.0",
+  "main": "index.js",
+  "dependencies": {
+    "express": "^4.17.1"
   }
-});
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-// Get all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
+// Install the express module
+npm install express
 
-// Get a single comment
-app.get('/comments/:id', (req, res) => {
-  const comment = comments.find(comment => comment.id === parseInt(req.params.id));
-  
-  if (!comment) {
-    res.status(404).send('The comment with the given ID was not found.');
-  } else {
-    res.json(comment);
-  }
-});
+// Run the server
+node index.js
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-// Get all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-// Get a single comment
-app.get('/comments/:id', (req, res) => {
-  const comment = comments.find(comment => comment.id === parseInt(req.params.id));
-  
-  if (!comment) {
-    res.status(404).send('The comment with the given ID was not found.');
-  } else {
-    res.json(comment);
-  }
-});
